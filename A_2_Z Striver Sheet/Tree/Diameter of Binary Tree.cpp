edade0f9cb3d69a70f678a00f402280a9b04721d
Nikk-05 <97886395+Nikk-05@users.noreply.github.com>
2022-09-27 18:03:56 +0530

    Create Diameter of Binary Tree.cpp
    https://leetcode.com/problems/diameter-of-binary-tree

---
+++ A_2_Z Striver Sheet/Tree/Diameter of Binary Tree.cpp
@@ -0,0 +1,17 @@
+ int height(TreeNode* root,int& maxi)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        int lh=height(root->left,maxi);
+        int rh=height(root->right,maxi);
+        maxi=max(maxi,lh+rh);
+        return max(lh,rh)+1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi=0;
+        height(root,maxi);
+        return maxi;
+    }
